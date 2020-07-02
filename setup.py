9e6e01858ddcc598536e1a8f028e48895d1563d1
Nicholas Romero <ncrmro@gmail.com>
2020-07-02 18:48:26 -0500

    chore: bumpt package

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="gear-torque-calc",
-    version="1.0.0",
+    version="1.0.1",
     description="Get torque output based on gear sizes",
     long_description=README,
     long_description_content_type="text/markdown",
